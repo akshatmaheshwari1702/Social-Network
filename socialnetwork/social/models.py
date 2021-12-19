008153bea411b6191691623a02e108f57a79b8e7
Akshat Maheshwari <maheshwariakshat482@gmail.com>
2021-12-20 00:28:44 +0530

    fixed bugs

--- socialnetwork/social/models.py
+++ socialnetwork/social/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # from socialnetwork.socialnetwork.settings import AUTH_PASSWORD_VALIDATORS
 
@@ -35,3 +37,14 @@ class UserProfile(models.Model):
         default="uploads/profile_pictures/default.png",
         blank=True,
     )
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profiles(sender, instance, **kwargs):
+    instance.profile.save()
